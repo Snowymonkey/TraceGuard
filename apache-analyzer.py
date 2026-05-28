375718217e338b066aff6832bfaefd73c344f2e4
Snowymonkey <143772406+Snowymonkey@users.noreply.github.com>
2026-05-28 12:15:42 -0500

    Finished Regex parsing of Apache log files and added more test files to .gitignore

--- apache-analyzer.py
+++ apache-analyzer.py
@@ -1,22 +1,28 @@
 import re
 
+HTTPMeaning = {'200' : 'OK, request succeeded', '304' : 'Not Modified', '400' : 'Bad Request', '401' : 'Unauthorized', '403' : 'Forbidden', '404' : 'Not Found', '410' : 'Gone'}
+pattern = re.compile(r'(\S+) - - \[(.*?)\] "(.*?)" (\d{3})')
+lineNumber = 1
 
-## Determine client IP / website
-with open('sample-logs/small_NASA_access_log_Jul95', 'r') as file:
+with open('sample-logs/medium_NASA_access_log_Jul95', 'r', errors='ignore') as file:
     for line in file:
+
+        match = pattern.match(line)
+    
+
+        if match:
+            client = match.group(1)
+            date = match.group(2)
+            request = match.group(3)
+            response_code = match.group(4)
+        else:
+            print("=============ERROR=============")
+            print(lineNumber, line)
         
-        splicedLine = line.split(" - - ", 1)
-        client = splicedLine[0]
-        date = splicedLine[1].split(" \"")[0]
-        HTTPCodes = splicedLine[1].split(" \"")[1].rsplit(" " ,2)[1]
-
-        print(client)
-        print(date)
-        print(HTTPCodes)
+        lineNumber += 1
         
+            
+            
 
-
-## Determine date and time of request
-
-
-## Status code of request
+        
+        
